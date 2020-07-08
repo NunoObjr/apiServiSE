78bf08ba45b394786860b42885687b977ad254f8
Nunoobjr <obertran.lima@fasminvestimentos.com.br>
2020-07-07 22:18:04 -0300

    origin

--- config/config.js
+++ config/config.js
@@ -1,29 +1,10 @@
 const env = process.env.NODE_ENV || 'dev';
 
-const config = ()=>{
-    switch (env){
-        case 'dev':
-            return{
-                APP_URL:3000,
-                bd_string:'mongodb+srv://nunoobjr:Junior123@servise-zrnqe.mongodb.net/<dbname>?retryWrites=true&w=majority',
-                jwt_pass:'admin123',
-                jwt_expires_in:'7d'
-            }
-        case 'hml':
-            return{
-                APP_URL:3000,
-                bd_string:'mongodb+srv://nunoobjr:Junior123@servise-zrnqe.mongodb.net/<dbname>?retryWrites=true&w=majority',
-                jwt_pass:'admin123',
-                jwt_expires_in:'7d'
-            }
-        case 'prod':
-            return{
-                APP_URL:3000,
-                bd_string:'mongodb+srv://nunoobjr:Junior123@servise-zrnqe.mongodb.net/<dbname>?retryWrites=true&w=majority',
-                jwt_pass:'admin123',
-                jwt_expires_in:'7d'
-        }
-    }
+const config = {
+    APP_URL:3000,
+    bd_string:'mongodb+srv://nunoobjr:Junior123@servise-zrnqe.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    jwt_pass:'admin123',
+    jwt_expires_in:'7d'
 }
 
 console.log(env.toUpperCase())
