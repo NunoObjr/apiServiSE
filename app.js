6722a70e52858ab7a347fc2182bf18d6acb02fcc
Nunoobjr <obertran.lima@fasminvestimentos.com.br>
2020-08-06 21:48:18 -0300

    socketFix

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
     socket.on('excluiuPrestador',data=>{
         socket.broadcast.emit('prestadorExcluido',data)
     })
-    socket.on('agendouServico',data=>{
+    socket.once('agendouServico',data=>{
         socket.broadcast.emit('servicoAgendado',data)
     });
     socket.on('usuarioCancelouAgendamento',data=>{
